4ed7a6338d1e0b9be5619c0b6f02aed821e513e8
Tanzil-Ripy <syedtanjilrippy@gmail.com>
2024-02-19 01:12:21 +0600

    connected with html

--- js.js
+++ js.js
@@ -1,8 +1,13 @@
 // btn click to open other tab
 const swichBtn =document.getElementById('banner-btn');
-const mainBody = document.getElementById('main-body');
+const mainBody = document.getElementById('mainBody');
+const headre = document.getElementById('header')
 swichBtn.addEventListener('click',function(){
+   
     if(mainBody.style.display === 'none'){
         mainBody.style.display ='block';
+        headre.style.display = 'none'
+    }else {
+        mainBody.style.display = 'none';
     }
 })
