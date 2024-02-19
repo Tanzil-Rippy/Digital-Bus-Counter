8c5dfe54db91c92ea5b887d11a5bcaf34c4bf65d
Tanzil-Ripy <syedtanjilrippy@gmail.com>
2024-02-19 17:04:22 +0600

    some function added

--- js.js
+++ js.js
@@ -11,3 +11,43 @@ swichBtn.addEventListener('click',function(){
         mainBody.style.display = 'none';
     }
 })
+
+// on click btn and action
+
+
+
+const seatAdded = document.getElementById('ticket-added')
+
+const A1 = document.getElementById('A1')
+
+let count =1
+const seatLeft = document.getElementById('ticket-seat-left');
+// let tseatLeft = seatLeft.innerText;
+// let fTSeatLeft = parseInt(tseatLeft)
+let nftSeatLeft =40
+let selectedSeat = 0
+console.log(nftSeatLeft)
+function seatBtnAction (){
+   
+
+
+    
+    if (count== 1){
+        A1.style.backgroundColor = '#1DD100';
+        count=0
+         nftSeatLeft= nftSeatLeft-1    
+         seatLeft.innerText =nftSeatLeft
+
+         selectedSeat  = selectedSeat+1  
+        seatAdded.innerText =selectedSeat
+        
+    }else{
+        A1.style.backgroundColor ='#FFFFFF'
+        count = 1
+        nftSeatLeft=nftSeatLeft +1
+        seatLeft.innerText =nftSeatLeft
+
+        selectedSeat  = selectedSeat-1
+        seatAdded.innerText = selectedSeat
+    }
+}
